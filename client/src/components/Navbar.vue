a5b7495ab952b8e4654219ca6a563615a2912057
Michelle <mwgstan@gmail.com>
2019-04-15 19:33:14 -0700

    Replaced vue-cli default app with crude header

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<!-- TODO
+    * add router links
+    * shopping cart needs functionality and state
+        * show number of items in cart in icon
+        * dropdown should show items in cart
+    * figure out how to get the dropdowns to turn into collapse elements when the screen size is small
+-->
+
+<template>
+    <b-navbar toggleable="sm" type="dark" variant="info">
+        <b-navbar-brand href="#">Kuma</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <hr />
+        <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto"> 
+                <b-nav-form>
+                    <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                </b-nav-form>
+
+                <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                    <template slot="button-content">
+                        <font-awesome-icon icon="user" /> 
+                        <span> Account</span>
+                    </template>
+            
+                    <b-dropdown-item href="#">My Orders</b-dropdown-item>
+                    <b-dropdown-item href="#">Settings</b-dropdown-item>
+
+                </b-nav-item-dropdown>
+
+                <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                    <template slot="button-content">
+                        <font-awesome-icon icon="shopping-cart" />
+                        <span> Cart</span> 
+                    </template>
+                    
+                    <b-dropdown-item href="#">TODO: put items here</b-dropdown-item>
+                    <b-dropdown-item href="#">To Checkout</b-dropdown-item>
+
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+
+
--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,24 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
 
-// for boostrap vue
-import BootstrapVue from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import BootstrapVue from 'bootstrap-vue'
 
-Vue.config.productionTip = false;
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUser);
+library.add(faShoppingCart);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
