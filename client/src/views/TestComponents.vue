de18f0bb2e94565075108b62931a82d5a65f4596
Steven Cruz <stiv.cruz6791@gmail.com>
2019-04-19 23:09:37 -0400

    Remove Draft Componenets
    Removed some draft components

--- client/src/views/TestComponents.vue
+++ client/src/views/TestComponents.vue
@@ -2,16 +2,12 @@
   <div class="container">
     <h1>This is the testing page of the components</h1>
     <hr>
-    <h4>Product Ribbon</h4>
-    <ProductRibbon/>
+
     <br>
     <h4>Product Card</h4>
     <ProductCard/>
     <br>
-    <br>
-    <h4>Product Grid - Will not use</h4>
-    <ProductGrid/>
-    <br>
+
     <h4>Featured Product</h4>
     <FeaturedProduct/>
 
@@ -20,13 +16,11 @@
   </div>
 </template>
 <script>
-import ProductRibbon from "@/components/ProductRibbon.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import ProductGrid from "@/components/ProductGrid.vue";
 import FeaturedProduct from "@/components/FeaturedProduct.vue";
 export default {
   name: "testComponents",
-  components: { ProductRibbon, ProductCard, ProductGrid, FeaturedProduct }
+  components: { ProductCard, FeaturedProduct }
 };
 </script>
 
