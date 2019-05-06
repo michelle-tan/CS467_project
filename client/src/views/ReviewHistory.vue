d915ac5b9159b2fba75e9b9c2aea37df712eab06
Michelle <mwgstan@gmail.com>
2019-05-06 13:13:32 -0700

    Add images to ReviewCard

--- client/src/views/ReviewHistory.vue
+++ client/src/views/ReviewHistory.vue
@@ -29,7 +29,12 @@ export default {
                 color: "brown",
                 rating: 3.5,
                 date: "January 1, 2019",
-                description: "cool guy, too smol"
+                description: "cool guy, too smol",
+                images: [
+                    "https://i.etsystatic.com/18460845/d/il/4ae257/1887124899/il_680x540.1887124899_1b5s.jpg",
+                    "https://i.etsystatic.com/18460845/c/1854/1472/54/0/il/1aa005/1706559720/il_680x540.1706559720_am80.jpg",
+                    "https://i.etsystatic.com/18460845/c/1506/1196/943/354/il/e43003/1839670678/il_680x540.1839670678_7czw.jpg"
+                    ]
             },
             {
                 title: "wrong size, one star",
