3599f205fe551249fc57d8292033563ff3eddb16
Michelle <mwgstan@gmail.com>
2019-06-09 12:00:19 -0500

    polish specific product page... -add review fetching, -make mobile friendly

--- client/src/views/SpecificProduct.vue
+++ client/src/views/SpecificProduct.vue
@@ -2,60 +2,57 @@
   <div class="container">
     <b-container class="bv-example-row">
       <b-row>
-        <b-col cols="7" class="bordera">
+        <b-col md="7" order="2" order-md="1" class="bordera">
           <ProductCarousel></ProductCarousel>
-
-          <br>
-          <br>
-          <hr>
-          <div>
-            <ProductDescBox :productObject="productObject"/>
-            <hr>
-          </div>
-          <div>
-            <h3>Reviews (Component E)</h3>
-            <b-alert v-model="showNotLoggedInAlert" variant="danger" dismissible>
-        Please be logged in to perform this action!
-          </b-alert>
-              <b-button @click="toggleReviewModal">Add review</b-button>
-        <b-modal
-            title="Add a review:"
-            v-model="showAddReviewModal"
-            centered
-        >
-            <ReviewForm
-                :handleSubmit="handleReviewSubmit"
-            />
-            <div slot="modal-footer"/>
-        </b-modal>
-        
-          </div>
         </b-col>
-        <b-col cols="5" class="bordera">
+          
+        <b-col md="5" order="1" order-md="2" class="bordera">
           <ProductInfoBox :productObject="productObject"/>
-          <br>
-          <hr>
-          <div>
-            <h3>Add to cart div (Component D)</h3>
-                <b-input
-                v-model="qty"
-                type="number"
-                />
-                <b-button @click="addToCart">Add to Cart</b-button>
-          </div>
-          <br>
-          <div id="boilerplate">
-            <hr>
+
+        </b-col>
+      </b-row>        
+        <b-row>
+          <b-col order="2" order-md="1" class="bordera">
+            <ProductDescBox :productObject="productObject"/>
             <h5 id="shipping">Shipping & Returns</h5>
             <p>
               Item ships from the USA within 2-5 business days.
               <br>Returns and exchanges accepted.
               <br>Exceptions may apply. Contact Seller for more information.
             </p>
-            <hr>
-          </div>
+          </b-col>
+        <b-col md="5" order="1" order-md="2"  class="bordera">
+          <h3>Add to cart div (Component D)</h3>
+            <b-input
+              v-model="qty"
+              type="number"
+              />
+              <b-button @click="addToCart">Add to Cart</b-button>
         </b-col>
-      </b-row>
+        </b-row>   
+          <b-row>
+            <b-col class="bordera">
+              <h3>Reviews (Component E)</h3>
+              <b-alert v-model="showNotLoggedInAlert" variant="danger" dismissible>
+                Please be logged in to perform this action!
+              </b-alert>
+              <b-button @click="toggleReviewModal">Add review</b-button>
+              <b-modal
+                  title="Add a review:"
+                  v-model="showAddReviewModal"
+                  centered
+              >
+                <ReviewForm
+                  :handleSubmit="handleReviewSubmit"
+                />
+                <div slot="modal-footer"/>
+              </b-modal>
+              <div v-for="(review,index) in productReviews" :key="review._id">
+                <ReviewCard :review="review" :index="index" :user_id="sessionData.userinfo.user_id"/>
+              </div>
+            </b-col>
+          </b-row>
+        
     </b-container>
     <div>
       <h3>Related Products(Product Ribbon)</h3>
@@ -83,9 +80,10 @@ import ProductDescBox from "@/components/ProductDescBox.vue";
 import ProductInfoBox from "@/components/ProductInfoBox.vue";
 import ProductCarousel from "@/components/ProductCarousel.vue";
 import ProductGrid from "@/components/ProductGrid.vue";
+import ReviewCard from "@/components/ReviewCard.vue"
 export default {
   name: "SpecificProduct",
-  components: { ProductDescBox, ProductInfoBox, ProductCarousel, ProductGrid, ReviewForm },
+  components: { ProductDescBox, ProductInfoBox, ProductCarousel, ProductGrid, ReviewForm, ReviewCard },
   props:{
     sessionData:Object
   },
@@ -96,7 +94,9 @@ export default {
       showAddReviewModal: false,
       showNotLoggedInAlert: false,
       relatedProducts: [],
-      valid: false
+      valid: false,
+      productReviews: []
+
     };
   },
   methods: {
@@ -140,6 +140,11 @@ export default {
           //console.log(res);
           this.$set(this.$data, "relatedProducts", res.data);
           this.valid = true;
+
+          axios.get(this.$hostname + '/reviews/byProduct/' + this.productObject._id).then(response=>{
+            this.$set(this.$data, "productReviews", response.data);
+          })
+
         })
         .catch(err => {
           console.log(err);
